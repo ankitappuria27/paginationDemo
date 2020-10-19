883acb6b93f48ee000568b39391017fc3b383e9d
Ankita Parasrampuria <ankitap@pune.smartek21.st21>
2020-10-19 11:21:52 +0530

    WIP:password authentication

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,12 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
 
 router.all('/*',function(req,res){
-  res.send('404');
+  res.send({status:404,message:"Not found"});
 });
 
 module.exports = router;
